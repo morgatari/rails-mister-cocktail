bf9e8ec6c0aae8f80d01ae0fde38b7ab9da2ba31
Fernando Morgado <github@fernandomorgado.eu>
2017-08-10 22:46:27 +0100

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "cocktails#index"
 
-  resources :cocktails, only: [:new, :index, :create, :show, :destroy] do
+  resources :cocktails, only: [:new, :index, :edit, :create, :show, :destroy] do
   resources :doses, only: [:new, :create, :destroy]
   end
 end
